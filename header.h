3fe02663eec50cbe2555866275e967b6c4933dc7
Tommyngo61 <tngo@pdx.edu>
2021-02-24 13:32:25 -0800

    I have added a mainUpdate README.md

---
+++ header.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <cmath>
+#include <ctime>
+#include <tuple>
+#include <math.h>
+#include <stdio.h>
+using namespace std;
