edaf51a3a926a8cb7c1008f99ec05d5eac9b36fc
Rio <riokimle123@gmail.com>
2021-03-08 21:10:33 -0800

    made a function name

--- main.cpp
+++ main.cpp
@@ -3,11 +3,14 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+int x ()
+{
+	cout << endl;
+}
 int main()
 {
  //  cout << "hello world" << endl;
-
+	x();
     cout << "helloooo" << endl;
     cout << "you fat" << endl;
     return 0;
