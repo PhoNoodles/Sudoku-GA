82ab32e4e59e39403991ba9656bbd1cf7b9b62fd
Michael <mhtran.hy@gmail.com>
2021-03-09 14:39:09 -0800

    refreshing main

--- main.cpp
+++ main.cpp
@@ -4,21 +4,8 @@
 #include <string>
 #include <vector>
 
-void testing()
-{
-    cout << "testing" << endl;
-}
-
-int x ()
-{
-	cout << endl;
-}
-
 int main()
 {
- //  cout << "hello world" << endl;
-	x();
-    cout << "helloooo" << endl;
-    cout << "you fat" << endl;
+
     return 0;
 }
