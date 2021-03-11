f49b228e50531ac63fe0332c97949c961bb98a83
Tommyngo61 <tngo@pdx.edu>
2021-03-10 18:40:35 -0800

    Did some fitness function stuff

--- chromosome.cpp
+++ chromosome.cpp
@@ -23,18 +23,36 @@ string Chromosome::makeState()
 	string temp;
 	int digit;
 	srand(time(NULL));
-	
+
 	for(int i = 0; i<8; ++i)
 	{
 		digit = rand () %9+1;
 		temp += to_string(digit);
 	}
-	
+
 	return temp;
 }
 
 int Chromosome::fitnessEval()
 {
-	return 1;
+	int fitness=216;
+	int already_checked=[1,2,3,4,5,6,7,8,9];
+	
+
+	//Loop for each row 
+	for(int i=0;i<81;i+=9)
+	{
+		//original number	
+		for(int j=0;j<9;++j)
+		{
+			//check for duplicate
+			for(int k=1;k<9;++k)
+			{
+				if(state[j+i]==state[k+i])
+					--fitness;
+			}
+		}
+	}
+
 
 }
